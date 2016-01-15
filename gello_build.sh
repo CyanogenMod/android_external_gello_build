1d71c2da9d869819409b05eb0b8db87ea7433eb1
jrizzoli <joey@cyanogenmoditalia.it>
2016-01-15 15:43:08 +0100

    gello-build: unbreak first-time usage
    Change-Id: Idaf36282df4445f028a286fd6bf4b315a474865f
    Signed-off-by: jrizzoli <joey@cyanogenmoditalia.it>

--- gello_build.sh
+++ gello_build.sh
@@ -49,7 +49,7 @@ function sync() {
         fi
 
         echo "Syncing now!"
-        gclient sync -n --no-nag-max
+        gclient sync -n --no-nag-max -j16
         return $?
     else
         return 0
@@ -66,6 +66,10 @@ function setup() {
 
     cd $SRC_GELLO
 
+    if [ ! -f $DONE_FILE ]; then
+        touch $DONE_FILE
+    fi
+
     . build/android/envsetup.sh
 
     if [ "$FAST" != true ] && [ -f $DONE_FILE ]; then
@@ -74,10 +78,6 @@ function setup() {
         return $?
     else
         return 0
-    fi
-
-    if [ ! -f $DONE_FILE ]; then
-        touch $DONE_FILE
     fi
 
     # If we don't have Google SDKs, get them
@@ -98,6 +98,15 @@ function compile() {
     local OUT_TARGET=$TOP_GELLO/Gello.apk
 
     cd $SRC_GELLO
+
+    # Gello "shell" builds only if we have WITH_GELLO_SOURCE == true ,
+    # this script is running, so it should already be true
+    # if we're just doing tests we may have not that as true, set it
+    # otherwise it won't hurt
+
+    if [ "$WITH_GELLO_SOURCE" != true ]; then
+        WITH_GELLO_SOURCE=true
+    fi
 
     # Make things
     ninja -C out/Release swe_android_browser_apk
@@ -167,7 +176,7 @@ function getdepot() {
 
     mkdir depot
     cd depot
-    git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
+    git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git depot
 }
 
 
