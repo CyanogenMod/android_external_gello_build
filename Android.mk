d31d198e9d1aa0ba0e3ff1976295aee982383002
jrizzoli <joey@cyanogenmoditalia.it>
2016-02-03 20:40:34 +0100

    gello-build: sourcebuild makefiles
    Change-Id: Ie76e1a1a2397de5d8516a41b2ebd5636ee5acf74
    Signed-off-by: jrizzoli <joey@cyanogenmoditalia.it>

---
+++ Android.mk
@@ -0,0 +1,33 @@
+#
+# Copyright (C) 2016 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Install the prebuilt gello apk.
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := Gello
+LOCAL_MODULE_CLASS := APPS
+LOCAL_CERTIFICATE := $(DEFAULT_SYSTEM_DEV_CERTIFICATE)
+
+ifeq ($(WITH_GELLO_SOURCE),true)
+# Build from source
+BUILD_GELLO := $(info $(shell bash $(LOCAL_PATH)/gello_build.sh 1>&2))
+LOCAL_SRC_FILES := Gello.apk
+endif
+
+include $(BUILD_PREBUILT)
